3ea7edda6b54751bf7ae1ca84846fbe4dad2db64
webdidi <44551050+webdidi@users.noreply.github.com>
2025-10-07 15:10:25 +0200

    Update Tag.tsx change ' by "

--- src/Tag.tsx
+++ src/Tag.tsx
@@ -1,12 +1,8 @@
 import figma, { html } from '@figma/code-connect/html'
 
-figma.connect('https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&t=LWaUohrWfTPApwK9-1'), {
+figma.connect("https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&t=LWaUohrWfTPApwK9-1"), {
   props: {
-    label: figma.textContent('Label')
+    label: figma.textContent("Label")
   },
-  example: ({ label }) => 
-    html`\
-  <p-tag>
-    ${label}
-  </p-tag>`
+  example: ({ label }) => html`<p-tag>${label}</p-tag>`
 })
