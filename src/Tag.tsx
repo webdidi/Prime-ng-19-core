98b6677e2eb0aa5bcef0f97d2d6e1958d61dcc12
webdidi <44551050+webdidi@users.noreply.github.com>
2025-10-07 15:25:21 +0200

    Delete src/Tag.tsx

--- src/Tag.tsx
+++
@@ -1,8 +0,0 @@
-import figma, { html } from '@figma/code-connect/html';
-
-figma.connect('https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&t=LWaUohrWfTPApwK9-1'), {
-  props: {
-    label: figma.textContent('Label')
-  },
-  example: (props) => html`<p-tag>${props.label}</p-tag>`
-})
