1d8a4ea45227683fb2cb3580a0c0a0a166686840
webdidi <44551050+webdidi@users.noreply.github.com>
2025-10-07 15:26:02 +0200

    Update Tag.tsx

--- src/Tag/Tag.tsx
+++ src/Tag/Tag.tsx
@@ -1,6 +1,6 @@
 import figma, { html } from '@figma/code-connect/html';
 
-figma.connect('https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&t=LWaUohrWfTPApwK9-1'), {
+figma.connect('https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&m=dev'), {
   props: {
     label: figma.textContent('Label')
   },
