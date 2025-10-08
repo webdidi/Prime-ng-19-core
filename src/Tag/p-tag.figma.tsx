4b9e551c6cfe5ad08afad52a9ae48ef2f8f51eb8
webdidi <44551050+webdidi@users.noreply.github.com>
2025-10-08 14:13:33 +0200

    Update p-tag.figma.tsx

--- src/Tag/p-tag.figma.tsx
+++ src/Tag/p-tag.figma.tsx
@@ -1,6 +1,6 @@
 import { html } from '@figma/code-connect/html';
 
-figma.connect('https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&m=dev'), {
+figma.connect('p-tag', 'https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&m=dev'), {
   props: {
     label: figma.textContent('Label')
   },
