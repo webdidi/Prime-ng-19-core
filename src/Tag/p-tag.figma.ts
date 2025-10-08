7f02bda51927d6162aba0dbd05fe2390002745ad
webdidi <44551050+webdidi@users.noreply.github.com>
2025-10-08 17:35:33 +0200

    Update p-tag.figma.ts

--- src/Tag/p-tag.figma.ts
+++ src/Tag/p-tag.figma.ts
@@ -4,5 +4,5 @@ figma.connect('https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Pri
   props: {
     label: figma.textContent('Label'),
   },
-  example: () => html`<p-tag></p-tag>,
+  example: ({label}) => html`<p-tag>${label}</p-tag>,
 })
