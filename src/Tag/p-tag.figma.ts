5e6285e63b80bb629215b2e2142b20bc4f9f3c0d
webdidi <44551050+webdidi@users.noreply.github.com>
2025-12-29 15:01:27 +0100

    Remove Figma connection code from p-tag.figma.ts

--- src/Tag/p-tag.figma.ts
+++ src/Tag/p-tag.figma.ts
@@ -1,8 +1,1 @@
-import figma, { html } from '@figma/code-connect/html';
 
-figma.connect('https://www.figma.com/design/9ERzMI9457zlkUpMs9x3Mf/GEDIPAS---Prime-NG-19-core?node-id=3352-5457&m=dev', {
-  props: {
-    label: figma.textContent('Label'),
-  },
-  example: ({label}) => html`<p-tag>${label}</p-tag>`,
-})
